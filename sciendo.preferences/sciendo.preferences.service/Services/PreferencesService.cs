b42ff843a5895fe235043eb096f7685a379d141e
oduma <tavi_duma2002@yahoo.co.uk>
2022-11-18 20:24:11 +0000

    grpc service working

---
+++ sciendo.preferences/sciendo.preferences.service/Services/PreferencesService.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.Xml;
+using Google.Protobuf.Collections;
+using sciendo.preferences.service;
+
+namespace sciendo.preferences.service.Services
+{
+    public class PreferencesService:Preferences.PreferencesBase
+    {
+        private ILogger<PreferencesService> _logger;
+
+        public PreferencesService(ILogger<PreferencesService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<GetPreferencesResponse> GetPreferences(GetPreferencesRequest request, Grpc.Core.ServerCallContext context) 
+        {
+            var result = new GetPreferencesResponse(); 
+            result.Artists.Add("abc");
+            result.Artists.Add("def");
+            result.Artists.Add("ghi");
+            return Task.FromResult(result);
+        }
+    }
+}
